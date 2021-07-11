2664cee9f63f5762b3a29733e8734ff081e6231b
assdfg0313 <50507520+assdfg0313@users.noreply.github.com>
2021-07-12 04:20:52 +0900

    add businees time version

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -72,7 +72,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "7f78122ede9e25c31bf76da7f600a753",
 "/": "7f78122ede9e25c31bf76da7f600a753",
-"main.dart.js": "ae84cf2c1323c39e6c993e07bf50e1f3",
+"main.dart.js": "a6af4568e09624309b998903e0d101ac",
 "manifest.json": "2deb9cb04446dbe92bd3e96e416a432d",
 "version.json": "a0cfd25587461a49402097ef5a754dfe"
 };
