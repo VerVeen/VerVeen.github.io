de1236ced449a1067e0f0d93c4c8f947166c9ae2
assdfg0313 <50507520+assdfg0313@users.noreply.github.com>
2021-07-21 16:39:12 +0900

    메뉴 버트 작동 안하게

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -72,7 +72,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "7f78122ede9e25c31bf76da7f600a753",
 "/": "7f78122ede9e25c31bf76da7f600a753",
-"main.dart.js": "154ab551bae30988f7f1004e5737e96d",
+"main.dart.js": "e68a7e4be82b3662da4caa7f64072967",
 "manifest.json": "2deb9cb04446dbe92bd3e96e416a432d",
 "version.json": "a0cfd25587461a49402097ef5a754dfe"
 };
